48456aabd68a002dd7764c217115da764949d667
jguyet <jguyet.weoohh@gmail.com>
2019-10-18 08:41:17 +0200

    binding parent and child component with infinity while

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { environment } from '../environments/environment';
 
 @Component({
@@ -10,5 +10,21 @@ export class AppComponent {
 
   public toto: string           = 'build';
   public production: boolean   = environment.production;
+  public currentNumber: number = 50;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  /** function called after emit on example component */
+  public computeFunction(event) {
+    console.log('ComputeEvent:', event);
+
+    /** After 1000ms increase currentNumber by 1 and force detect changes */
+    const parent = this;
+    setTimeout(() => {
+        console.log(parent.currentNumber);
+        parent.currentNumber += 1;
+        parent.changeDetectorRef.detectChanges();
+    }, 1000);
+  }
 
 }
--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
   styleUrls: ['./example.component.scss']
 })
-export class ExampleComponent implements OnInit {
+export class ExampleComponent implements OnInit, OnChanges {
+
+    /** Reception of binding attr */
+    @Input('number') number: number = 0;
+
+    /** Binding EventEmitter for parent */
+    @Output('computeEvent') computeEventEmitter: EventEmitter<number> = new EventEmitter();
 
     public text: string = 'value';
 
@@ -20,11 +26,24 @@ export class ExampleComponent implements OnInit {
 
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+    }
+
+    /** After change detector detect new change call this with simpleChanges pattern */
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.number && changes.number.currentValue != changes.number.previousValue) {
+            this.compute();
+        }
+    }
+    
+    public compute() {
+        console.log('number:', this.number);
+        this.computeEventEmitter.emit(this.number + 1);
+    }
 
     // EVENT BINDING FUNCTION :
     public click(event) {
-        console.log(event, );
+        console.log(event);
     }
 
 }
