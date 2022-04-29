ac601e07d9b6a5d7ff0856af7ca6f8ba7c54e528
jguyet <gygydu19@hotmail.fr>
2022-04-29 14:15:48 +0200

    login animation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/security/auth.service';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('logged', [
+        state('false', style({
+            backgroundColor: 'black',
+            color: 'white'
+        })),
+        state('true', style({
+            backgroundColor: 'white',
+            color: 'black'
+        })),
+        transition('false => true', [
+            animate('1s')
+        ]),
+        transition('true => false', [
+            animate('0.5s')
+        ])
+    ])
+  ]
 })
 export class LoginComponent {
 
