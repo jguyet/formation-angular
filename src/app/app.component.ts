cdf929da6561d17cd028a583f2c553dc965ab2cd
jguyet <jguyet.weoohh@gmail.com>
2024-12-11 16:58:38 +0100

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { CardComponent } from './shared/components/card/card.component';
 
 @Component({
     selector: 'app-root',
     imports: [
         RouterModule,
-        HeaderComponent,
-        CardComponent
+        HeaderComponent
     ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
